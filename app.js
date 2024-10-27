8ba1c1da65033f0cd5702f5fdd818894d1fba89a
DevloperMM <mangalmurtivarshney2810@gmail.com>
2024-10-27 12:12:12 +0530

    Frontend Structure Designed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import ejsMate from "ejs-mate";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,6 +11,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+app.engine("ejs", ejsMate);
 
 import portalRouter from "./routes/portal.route.js";
 app.use("/", portalRouter);
