f552c878d8927440e9a246382dfa57b87e77b5d3
DevloperMM <mangalmurtivarshney2810@gmail.com>
2024-10-28 11:50:12 +0530

    Navbar styling ready

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
-import ejsMate from "ejs-mate";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,7 +10,6 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
-app.engine("ejs", ejsMate);
 
 import portalRouter from "./routes/portal.route.js";
 app.use("/", portalRouter);
